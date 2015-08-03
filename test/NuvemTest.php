6ee83e86e9a7cadb8fe403def779d17a2a2b48b9
castrolol <lu4n.k4str0@gmail.com>
2015-08-03 18:22:54 -0400

    Adicionado testes para cobertura do aeroporto, divido testes de geracao de mapa e quantidade de dias

--- test/NuvemTest.php
+++ test/NuvemTest.php
@@ -5,7 +5,7 @@ use App\Nuvem;
 class NuvemTest extends PHPUnit_Framework_TestCase
 {
 
-	public function testPrimeiroDia()
+	public function testGeracaoMapa()
 	{
 		$mapaInicio = [
 			['.', '.', '*', '.', '.', '.', '*', '*'],
@@ -35,7 +35,60 @@ class NuvemTest extends PHPUnit_Framework_TestCase
 		$actualMap = $nuvem->getActualMap();
 
 		$this->assertEquals($mapaFinal, $actualMap);
+		
+	}
+	
+	public function testeQuantidadeDias(){
+		
+		$mapaInicio = [
+			['.', '.', '*', '.', '.', '.', '*', '*'],
+			['.', '*', '*', '.', '.', '.', '.', '.'],
+			['*', '*', '*', '.', 'A', '.', '.', 'A'],
+			['.', '*', '.', '.', '.', '.', '.', '.'],
+			['.', '*', '.', '.', '.', '.', 'A', '.'],
+			['.', '.', '.', 'A', '.', '.', '.', '.'],
+			['.', '.', '.', '.', '.', '.', '.', '.']
+		];
+
+		$nuvem = new Nuvem($mapaInicio);
+
+		$nuvem->advanceDay();
+		$nuvem->advanceDay();
+
 		$this->assertEquals(3, $nuvem->getDaysElapsed());
+		
+	}
+
+	public function testCoberturaAviao(){
+		
+		
+		$mapaInicio = [
+			['.', '.', '*', '.', '.', '.', '*', '*'],
+			['.', '*', '*', '.', '.', '.', '.', '.'],
+			['*', '*', '*', '.', 'A', '.', '.', 'A'],
+			['.', '*', '.', '.', '.', '.', '.', '.'],
+			['.', '*', '.', '.', '.', '.', 'A', '.'],
+			['.', '.', '.', 'A', '.', '.', '.', '.'],
+			['.', '.', '.', '.', '.', '.', '.', '.']
+		];
+		
+		$nuvem = new Nuvem($mapaInicio);
+
+
+
+		$airports = $nuvem->getCoveredAirports();
+		$this->assertEquals(sizeof($airports), 0);
+
+		$nuvem->advanceDay();
+
+		$airports = $nuvem->getCoveredAirports();
+		$this->assertEquals(sizeof($airports), 0);
+
+		$nuvem->advanceDay();
+
+		$airports = $nuvem->getCoveredAirports();
+		$this->assertEquals(sizeof($airports), 2);
+
 	}
 
 }
